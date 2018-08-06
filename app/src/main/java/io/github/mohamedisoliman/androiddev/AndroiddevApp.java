81491198492c9ce8215345fa3ff46b003e7ccc4b
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2018-08-07 00:38:38 +0200

    add crashlytics

--- app/src/main/java/io/github/mohamedisoliman/androiddev/AndroiddevApp.java
+++ app/src/main/java/io/github/mohamedisoliman/androiddev/AndroiddevApp.java
@@ -1,6 +1,8 @@
 package io.github.mohamedisoliman.androiddev;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Mohamed Ibrahim on 8/1/18.
@@ -10,5 +12,7 @@ public class AndroiddevApp extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
+
+    Fabric.with(this, new Crashlytics());
   }
 }
